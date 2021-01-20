bc1b5e7d7903f8ada87e285cf39824cef274b4b7
Eric <eric.oyanadel@nbcuni.com>
2021-01-20 13:53:50 -0500

    first commit and setup config with sanity to grapqhl

---
+++ 100days-gatsby2021/gatsby-config.js
@@ -0,0 +1,31 @@
+require('dotenv').config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: '100DaysGatsby2021',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'k0b45exx',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
