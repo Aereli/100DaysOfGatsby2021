02f24b21ac25b5b48bb7dab06a711c68f2c27070
Eric <eric.oyanadel@nbcuni.com>
2021-01-20 13:53:21 -0500

    setup sanity cms ans location schema

---
+++ sanity/schemas/location.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'locations',
+  //visiblie title
+  // title: 'locations',
+  type: 'document',
+  fields: [
+    {
+      name: 'city',
+      title: 'City Name',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'City Description',
+      type: 'string',
+    },
+  ],
+}
