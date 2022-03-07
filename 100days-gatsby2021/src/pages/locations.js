69c30b2c8631e1ec33282e7c592526d4f5bbb020
Eric Oyanadel <eric.oyanadel@slalom.com>
2022-03-07 17:56:12 -0500

    comment prior sanity setup

--- 100days-gatsby2021/src/pages/locations.js
+++ 100days-gatsby2021/src/pages/locations.js
@@ -8,28 +8,31 @@ import GetLatestData from '../functions/getLatestData'
 const Locations = () => {
 
 const results = GetLatestData()
-console.log('res from loca', results)  
-const data = useStaticQuery(graphql`
-    query {
-      locations: allSanityLocations {
-        nodes {
-          id
-          city
-          description
-        }
-      }
-    }
-  `)
-  const cities = data.locations.nodes
+
+// const data = useStaticQuery(graphql`
+//     query {
+//       locations: allSanityLocations {
+//         nodes {
+//           id
+//           city
+//           description
+//         }
+//       }
+//     }
+//   `)
+  // const cities = data.locations.nodes
+
   return (
     <Layout>
       <SEO ttile="Locations" />
-      {results && results.map((location) => (
+      {results ? results.map((location) => (
         <div key={location._id} styles={{ margin: `1rem` }}>
           {/* <Link to={`/location/${location.city}`}>{location.city}</Link> */}
           <Link to={`/location/${location.city}`}>{location.city}</Link>
         </div>
-      ))}
+      ))
+        : <p>Loading...</p>
+    }
     </Layout>
   )
 }
