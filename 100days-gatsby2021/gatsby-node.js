79cffbee1939a0cb70f3161306efab6b7141abfc
Eric Oyanadel <eric.oyanadel@slalom.com>
2022-03-07 19:56:07 -0500

    setupd the sanity user playground

--- 100days-gatsby2021/gatsby-node.js
+++ 100days-gatsby2021/gatsby-node.js
@@ -3,18 +3,33 @@ const path = require(`path`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const { data } = await graphql(`
-    query {
-      allSanityLocations {
-        nodes {
-          id
-          city
-          description
-        }
-      }
-    }
-  `)
-  data.allSanityLocations.nodes.forEach((location) => {
+  const res = await fetch(process.env.GATSBY_SANITY_GRAPHQL, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        query:
+        `query {
+            allLocations {
+              _id
+              city
+              description
+              image {
+                asset {
+                  url
+                  metadata {
+                    lqip
+                  }
+                }
+              }
+            }
+          }`
+    })
+})
+const locations = await res.json()
+
+locations.data.allLocations.forEach((location) => {
     createPage({
       path: `/location/${location.city}`,
       component: path.resolve('./src/templates/location-template.js'),
@@ -25,3 +40,28 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const { data } = await graphql(`
+//     query {
+//       allSanityLocations {
+//         nodes {
+//           id
+//           city
+//           description
+//         }
+//       }
+//     }
+//   `)
+//   data.allSanityLocations.nodes.forEach((location) => {
+//     createPage({
+//       path: `/location/${location.city}`,
+//       component: path.resolve('./src/templates/location-template.js'),
+//       context: {
+//         name: location.city,
+//         // slug: location.city,
+//       },
+//     })
+//   })
+// }
